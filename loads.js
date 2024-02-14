e64a1142c8c7070f3cc36ee0f4faba697312ddfd
xxinxinxin <149231884+xxinxinxin@users.noreply.github.com>
2024-02-14 17:07:00 +0800

    Update loads.js

--- loads.js
+++ loads.js
@@ -38,7 +38,7 @@ if (screen.width >= 768) {
 			// cdnPath: "https://live2d-api.vercel.app/"
 			//apiPath: "https://api3.fghrsh.net/live2d/"
 			// cdnPath: "https://gitee.com/ajream/live2d_api/raw/master/"
-			cdnPath: "https://cdn.staticaly.com/gh/xxinxinxin/live2d_api/master/"
+			cdnPath: "https://cdn.staticaly.com/gh/xxinxinxin/live2d_api/main/"
 		});
 	});
 }
